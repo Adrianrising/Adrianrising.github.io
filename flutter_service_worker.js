d3249148050bc8653f77f002e6bc71ff259bc5d1
BhoneMyintThu <112772850+Adrianrising@users.noreply.github.com>
2023-10-25 13:58:42 +0630

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "de2bc343eee6267e9b30624787abdf7a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3a99cb367a54825afef8e4841e43bc7b",
-"/": "3a99cb367a54825afef8e4841e43bc7b",
+"index.html": "11e52c98a51fd3310e45923444ee79d3",
+"/": "11e52c98a51fd3310e45923444ee79d3",
 "main.dart.js": "8fe1cef7ad62f9f8e2e3c5753e7fe737",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"};
